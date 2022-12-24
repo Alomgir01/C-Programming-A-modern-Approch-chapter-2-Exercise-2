7d89ec06f79508e8e9344b60c3841e3660f371ee
AlomgirH <102249051+Alomgir01@users.noreply.github.com>
2022-12-24 14:02:56 +0600

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,14 @@
+/* Computes the dimensional weight of a box from input provided by the user*/
+ #include<stdio.h>
+ int main(void)
+ {
+     int heigth,length,width,volume;
+     heigth=10;
+     length=12;
+     width=15;
+volume= heigth*length*width;
+
+     printf("Volume (cubic inches):%d\n",volume);
+
+ return 0;
+ }
