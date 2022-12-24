0ef35013c2153484eed3e70af057cf0b4fbdae58
AlomgirH <102249051+Alomgir01@users.noreply.github.com>
2022-12-24 14:03:34 +0600

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(void)
+{
+    int Alomgir, Rifat, age;
+    Alomgir=21.0f;
+    Rifat=20.0f;
+   age=(Alomgir+Rifat);
+
+    printf("%d\n\n",age);
+
+
+   {
+        float Alomgir, Rifat,age;
+    printf("Enter Alomgir's age: ");
+    scanf("%f",&Alomgir);
+    printf("Enter Rifat's age: ");
+    scanf("%f",&Rifat);
+
+    age= Alomgir+Rifat;
+    printf("Total age are: %f",age);
+
+   }
+
+}
+
